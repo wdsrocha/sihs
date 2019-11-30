cabfc8c46ca1dbd290038a53ebe0b4d840e2ec62
nataliacavalcantex <nataliacavalcantex@Mac-mini-de-Natalia.local>
2019-11-30 14:06:03 -0400

    new routes

--- app.py
+++ app.py
@@ -2,26 +2,33 @@
 # -*- coding: utf-8 -*-
 from flask_cors import CORS
 from flask import Flask, request,jsonify, make_response, session
-from flask_sqlalchemy import SQLAlchemy #comunicacao com o banco
+from flask_sqlalchemy import SQLAlchemy
 from random import randint
 from sqlalchemy import DateTime
 import qrcode
 import pyqrcode
 import datetime
+import secrets
+import png
+from functools import wraps
 from flask import send_file
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecret'
+
+from sqlalchemy import create_engine
+engine = create_engine('postgres://postgres:123@localhost:5555/db_flask')
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:123@localhost:5555/db_flask'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 CORS(app,origins='*')
 
 db = SQLAlchemy(app)
 
+#DATABASE
 class Users(db.Model):
     __tablename__ = 'db_users'
     
-    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100),unique=True,nullable=False)
     device_id = db.Column(db.Integer,db.ForeignKey('db_device.id'),nullable=False) 
 
@@ -29,7 +36,8 @@ class Users(db.Model):
     
     def __repr__(self):
         return f"User('{self.username}', '{self.id}')"
-# insert into db_user values(11,'nat')
+
+    
 
 class Device(db.Model):
     __tablename__ = 'db_device'
@@ -43,7 +51,7 @@ class Device(db.Model):
    
     def __repr__(self):
         return f"Device('{self.serial}', '{self.id}')"
-#insert into db_device values(1,'camera',1,2); 
+
 
 class Guest(db.Model):
     __tablename__ = 'db_guest'
@@ -55,7 +63,6 @@ class Guest(db.Model):
     
     def __repr__(self):
         return f"Guest('{self.username}', '{self.id}')"
-# insert into db_alloweduser values (3,'diogo','91c863584c871f224cd15215999b62c5',2)
 
 class Invitation(db.Model):
     __tablename__ = 'db_invitaion'
@@ -71,86 +78,101 @@ class Invitation(db.Model):
     
     def __repr__(self):
         return f"Invitation('{self.username}', '{self.id}')"
-#insert into db_event values(1,'festa',3,2)  
 
-#routes
+
+
+
+#ROUTES
 @app.route('/')
-def main():
-    return 'oi'
+def hello():
+    return 'Hello'
 
-# User register 
-@app.route('/register')
+## User register 
+@app.route('/register',methods=['POST'])
 def registerUser():
-    data= request.get_json()
-    if(db.session.query(Users.id).filter_by(id=data['id']).scalar() is not None):
-        return 'This user already exist'
-    new_user=Users(username=data['username'],device_id=data['dispositivo'])
+    data = request.get_json()
+    if(db.session.query(Users.username).filter_by(username=data['username']).scalar() is not None):
+        return 'This username already exist!'
+
+    new_user=Users(id=data['id'],username=data['username'],device_id=data['dispositivo'])
     db.session.add(new_user)
     db.session.commit()
     response = make_response(jsonify({'message': 'User registred sucessfuly', 'username':new_user.username}))
     return response
 
-#get devices
-@app.route('/devices/<int:device_id>')
-def get_devices(devices_id):
-    device=Device.query.filter_by(id=(device_id)).first()
+## Get devices
+@app.route('/devices')
+def get_devices():
+    devices= Device.query.all()
+    output = []
+    for device in devices:
+        device_data = {'serial': device.serial}
 
-    if(device ==  None):
-        return 'No devices registered'
-    output={
-        'serial':device.serial
-    }
-    return jsonify(output)
+        output.append(device_data)
+        
+    response = make_response(jsonify({'Devices': output}))
+    return response
 
-#register guest
-@app.route('/guest')
+## Guest register 
+@app.route('/guest',methods=['POST'] )
 def registerGuest():
     data= request.get_json()
     if(db.session.query(Guest.email).filter_by(email=data['email']).scalar() is not None):
-        return 'This user already exist'
-    new_guest=Users(email=data['email'])
+        return 'This guest is already registred'
+
+    new_guest=Guest(id=data['id'],email=data['email'])
     db.session.add(new_guest)
     db.session.commit()
-    response = make_response(jsonify({'message': 'User registred sucessfuly', 'username':new_user.username}))
+    response = make_response(jsonify({'message': 'Guest registred sucessfuly', 'username':new_guest.email}))
     return response
 
-#generate an invitation
-@app.route('/invite')
-def  get_invitation():
+## Generate an invitation
+@app.route('/invite',methods=['POST'])
+
+def createInvitation():
     data = request.get_json()
     if(db.session.query(Invitation.id).filter_by(id=data['id']).scalar() is not None):
-        return 'This invitation already exist'
-    new_invitation=Invitation(user_id=data['user_id'],guest_id=data['guest_id'],device_id=data['device_id'],qrcode=data['qrcode'])
+         return 'This invitation already exist'
+
+    new_invitation=Invitation(id=data['id'],qrcode=generateQRImage(),user_id=data['user'],guest_id=data['guest'],device_id=data['device'])
     db.session.add(new_invitation)
     db.session.commit()
     response = make_response(jsonify({'message': 'Invitaion created!', 'qrcode':new_invitation.qrcode}))
     return response
 
+## Get invitation
+@app.route('/invitations')
+def get_invitations():
+    
+    invitations= Invitation.query.all()
+    output=[]
+    for invitation in invitations:
+        invitation_data ={"id":invitation.id,"qrcode":invitation.qrcode,"user":invitation.user_id,"guest":invitation.guest_id,"device":invitation.device_id}
+        output.append(invitation_data)
+        
+    response = make_response(jsonify({'Invitations': output}))
+    return response
+
+
+@app.route('/teste')
+#  userList = users.query.join(friendships, users.id==friendships.user_id).add_columns(users.id, users.userName, users.userEmail, friendships.id, friendships.user_id, friendships.friend_id).filter(friendships.friend_id == userID)
+def main():
+    query= Users.query.join(Invitation, Users.id==Invitation.user_id).add_columns(Users.id, Users.username, Invitation.id, Invitation.user_id, Invitation.qrcode).filter(Invitation.user_id == userID)
+    print(query.statement)
+    return 'rola'
+    # output=[]
+    # for invitation in invitations:
+    #     invitation_data ={"id":invitation.id,"qrcode":invitation.qrcode,"user":invitation.user_id,"guest":invitation.guest_id,"device":invitation.device_id}
+    #     output.append(invitation_data)
+        
+    # response = make_response(jsonify({'Invitations': output}))
+    # return response
+
+## Generate qrcode
 @app.route("/qrgenerator")
 def generateQRImage():
-    link_to_post = "http://127.0.0.1:5000/"
-    url = pyqrcode.create(link_to_post)
+    code = secrets.token_hex(16)
+    url = pyqrcode.create(code)
     url.png('url.png', scale=8)
     print(url.terminal())
-    return 'Printing QR code'
-
-@app.route('/validation/<validation_code>')
-def validate_qrcode(validation_code):
-    if(db.session.query(AllowedUser.id).filter_by(validation=validation_code).scalar() is not None):
-        return 'False'
-    return 'True'
-    
-
-# @app.route('/invitation', methods=['POST'])
-# def invite_guest():
-#     data = request.get_json()
-#     if(db.session.query(AllowedUser.username).filter_by(username=data['name']).scalar() is not None):
-#         return 'User ja cadastrado!'
-#     new_user = AllowedUser(id = randint(0,100),username=data['name'],validation=data['validation_code'],user_id =data['user_id'])
-#     db.session.add(new_user)
-#     db.session.commit()
-#     response = make_response(jsonify({'message': 'User Cadastrado!', 'id':new_user.id}))
-#     response.headers['Access-Control-Allow-Origin'] = '*'
-#     return response
-
-    
+    return code
