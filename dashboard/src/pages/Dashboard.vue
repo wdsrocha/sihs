e3553762c7c8763a3487983f2be688ad67d3311e
dayvsonsantos <dayvsondossantossilva@gmail.com>
2019-12-01 01:05:13 -0400

    update graphic

--- dashboard/src/pages/Dashboard.vue
+++ dashboard/src/pages/Dashboard.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-
+    <div class="row">
+      <div class="col-12">
+        <card type="nav-tabs" class="text-center">
+          <div slot="header" class="card-header-primary">Featured</div>
+          <h4 class="card-title">Special title treatment</h4>
+          <p
+            class="card-text"
+          >With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <div slot="footer" class="text-muted">2 days ago</div>
+        </card>
+      </div>
+    </div>
     <div class="row">
       <div class="col-12">
         <card type="chart">
@@ -8,21 +20,29 @@
             <div class="row">
               <div class="col-sm-6" :class="isRTL ? 'text-right' : 'text-left'">
                 <h5 class="card-category">{{$t('dashboard.totalShipments')}}</h5>
-                <h2 class="card-title">{{$t('dashboard.performance')}}</h2>
+								<h2 class="card-title">Numero de acessos ao sistema durante o ano</h2>
+                <!-- <h2 class="card-title">{{$t('dashboard.performance')}}</h2> -->
               </div>
               <div class="col-sm-6">
-                <div class="btn-group btn-group-toggle"
-                     :class="isRTL ? 'float-left' : 'float-right'"
-                     data-toggle="buttons">
-                  <label v-for="(option, index) in bigLineChartCategories"
-                         :key="option"
-                         class="btn btn-sm btn-primary btn-simple"
-                         :class="{active: bigLineChart.activeIndex === index}"
-                         :id="index">
-                    <input type="radio"
-                           @click="initBigChart(index)"
-                           name="options" autocomplete="off"
-                           :checked="bigLineChart.activeIndex === index">
+                <div
+                  class="btn-group btn-group-toggle"
+                  :class="isRTL ? 'float-left' : 'float-right'"
+                  data-toggle="buttons"
+                >
+                  <label
+                    v-for="(option, index) in bigLineChartCategories"
+                    :key="option"
+                    class="btn btn-sm btn-primary btn-simple"
+                    :class="{active: bigLineChart.activeIndex === index}"
+                    :id="index"
+                  >
+                    <input
+                      type="radio"
+                      @click="initBigChart(index)"
+                      name="options"
+                      autocomplete="off"
+                      :checked="bigLineChart.activeIndex === index"
+                    />
                     {{option}}
                   </label>
                 </div>
@@ -30,14 +50,15 @@
             </div>
           </template>
           <div class="chart-area">
-            <line-chart style="height: 100%"
-                        ref="bigChart"
-                        chart-id="big-line-chart"
-                        :chart-data="bigLineChart.chartData"
-                        :gradient-colors="bigLineChart.gradientColors"
-                        :gradient-stops="bigLineChart.gradientStops"
-                        :extra-options="bigLineChart.extraOptions">
-            </line-chart>
+            <line-chart
+              style="height: 100%"
+              ref="bigChart"
+              chart-id="big-line-chart"
+              :chart-data="bigLineChart.chartData"
+              :gradient-colors="bigLineChart.gradientColors"
+              :gradient-stops="bigLineChart.gradientStops"
+              :extra-options="bigLineChart.extraOptions"
+            ></line-chart>
           </div>
         </card>
       </div>
@@ -47,16 +68,19 @@
         <card type="chart">
           <template slot="header">
             <h5 class="card-category">{{$t('dashboard.totalShipments')}}</h5>
-            <h3 class="card-title"><i class="tim-icons icon-bell-55 text-primary "></i> 763,215</h3>
+            <h3 class="card-title">
+              <i class="tim-icons icon-bell-55 text-primary"></i> 763,215
+            </h3>
           </template>
           <div class="chart-area">
-            <line-chart style="height: 100%"
-                        chart-id="purple-line-chart"
-                        :chart-data="purpleLineChart.chartData"
-                        :gradient-colors="purpleLineChart.gradientColors"
-                        :gradient-stops="purpleLineChart.gradientStops"
-                        :extra-options="purpleLineChart.extraOptions">
-            </line-chart>
+            <line-chart
+              style="height: 100%"
+              chart-id="purple-line-chart"
+              :chart-data="purpleLineChart.chartData"
+              :gradient-colors="purpleLineChart.gradientColors"
+              :gradient-stops="purpleLineChart.gradientStops"
+              :extra-options="purpleLineChart.extraOptions"
+            ></line-chart>
           </div>
         </card>
       </div>
@@ -64,15 +88,18 @@
         <card type="chart">
           <template slot="header">
             <h5 class="card-category">{{$t('dashboard.dailySales')}}</h5>
-            <h3 class="card-title"><i class="tim-icons icon-delivery-fast text-info "></i> 3,500€</h3>
+            <h3 class="card-title">
+              <i class="tim-icons icon-delivery-fast text-info"></i> 3,500€
+            </h3>
           </template>
           <div class="chart-area">
-            <bar-chart style="height: 100%"
-                       chart-id="blue-bar-chart"
-                       :chart-data="blueBarChart.chartData"
-                       :gradient-stops="blueBarChart.gradientStops"
-                       :extra-options="blueBarChart.extraOptions">
-            </bar-chart>
+            <bar-chart
+              style="height: 100%"
+              chart-id="blue-bar-chart"
+              :chart-data="blueBarChart.chartData"
+              :gradient-stops="blueBarChart.gradientStops"
+              :extra-options="blueBarChart.extraOptions"
+            ></bar-chart>
           </div>
         </card>
       </div>
@@ -80,15 +107,18 @@
         <card type="chart">
           <template slot="header">
             <h5 class="card-category">{{$t('dashboard.completedTasks')}}</h5>
-            <h3 class="card-title"><i class="tim-icons icon-send text-success "></i> 12,100K</h3>
+            <h3 class="card-title">
+              <i class="tim-icons icon-send text-success"></i> 12,100K
+            </h3>
           </template>
           <div class="chart-area">
-            <line-chart style="height: 100%"
-                        chart-id="green-line-chart"
-                        :chart-data="greenLineChart.chartData"
-                        :gradient-stops="greenLineChart.gradientStops"
-                        :extra-options="greenLineChart.extraOptions">
-            </line-chart>
+            <line-chart
+              style="height: 100%"
+              chart-id="green-line-chart"
+              :chart-data="greenLineChart.chartData"
+              :gradient-stops="greenLineChart.gradientStops"
+              :extra-options="greenLineChart.extraOptions"
+            ></line-chart>
           </div>
         </card>
       </div>
@@ -99,11 +129,13 @@
           <template slot="header">
             <h6 class="title d-inline">{{$t('dashboard.tasks', {count: 5})}}</h6>
             <p class="card-category d-inline">{{$t('dashboard.today')}}</p>
-            <base-dropdown menu-on-right=""
-                           tag="div"
-                           title-classes="btn btn-link btn-icon"
-                           aria-label="Settings menu"
-                           :class="{'float-left': isRTL}">
+            <base-dropdown
+              menu-on-right
+              tag="div"
+              title-classes="btn btn-link btn-icon"
+              aria-label="Settings menu"
+              :class="{'float-left': isRTL}"
+            >
               <i slot="title" class="tim-icons icon-settings-gear-63"></i>
               <a class="dropdown-item" href="#pablo">{{$t('dashboard.dropdown.action')}}</a>
               <a class="dropdown-item" href="#pablo">{{$t('dashboard.dropdown.anotherAction')}}</a>
@@ -127,40 +159,41 @@
   </div>
 </template>
 <script>
-  import LineChart from '@/components/Charts/LineChart';
-  import BarChart from '@/components/Charts/BarChart';
-  import * as chartConfigs from '@/components/Charts/config';
-  import TaskList from './Dashboard/TaskList';
-  import UserTable from './Dashboard/UserTable';
-  import config from '@/config';
+import LineChart from "@/components/Charts/LineChart";
+import BarChart from "@/components/Charts/BarChart";
+import * as chartConfigs from "@/components/Charts/config";
+import TaskList from "./Dashboard/TaskList";
+import UserTable from "./Dashboard/UserTable";
+import config from "@/config";
 
-  export default {
-    components: {
-      LineChart,
-      BarChart,
-      TaskList,
-      UserTable
-    },
-    data() {
-      return {
-        bigLineChart: {
-          allData: [
-            [100, 70, 90, 70, 85, 60, 75, 60, 90, 80, 110, 100],
-            [80, 120, 105, 110, 95, 105, 90, 100, 80, 95, 70, 120],
-            [60, 80, 65, 130, 80, 105, 90, 130, 70, 115, 60, 130]
-          ],
-          activeIndex: 0,
-          chartData: null,
-          extraOptions: chartConfigs.purpleChartOptions,
-          gradientColors: config.colors.primaryGradient,
-          gradientStops: [1, 0.4, 0],
-          categories: []
-        },
-        purpleLineChart: {
-          extraOptions: chartConfigs.purpleChartOptions,
-          chartData: {
-            labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
-            datasets: [{
+export default {
+  components: {
+    LineChart,
+    BarChart,
+    TaskList,
+    UserTable
+  },
+  data() {
+    return {
+      bigLineChart: {
+        allData: [
+          [100, 70, 90, 70, 85, 60, 75, 60, 90, 80, 110, 100],
+          [80, 120, 105, 110, 95, 105, 90, 100, 80, 95, 70, 120],
+          [60, 80, 65, 130, 80, 105, 90, 130, 70, 115, 60, 130]
+        ],
+        activeIndex: 0,
+        chartData: null,
+        extraOptions: chartConfigs.purpleChartOptions,
+        gradientColors: config.colors.primaryGradient,
+        gradientStops: [1, 0.4, 0],
+        categories: []
+      },
+      purpleLineChart: {
+        extraOptions: chartConfigs.purpleChartOptions,
+        chartData: {
+          labels: ["JUL", "AUG", "SEP", "OCT", "NOV", "DEC"],
+          datasets: [
+            {
               label: "Data",
               fill: true,
               borderColor: config.colors.primary,
@@ -168,23 +201,25 @@
               borderDash: [],
               borderDashOffset: 0.0,
               pointBackgroundColor: config.colors.primary,
-              pointBorderColor: 'rgba(255,255,255,0)',
+              pointBorderColor: "rgba(255,255,255,0)",
               pointHoverBackgroundColor: config.colors.primary,
               pointBorderWidth: 20,
               pointHoverRadius: 4,
               pointHoverBorderWidth: 15,
               pointRadius: 4,
-              data: [80, 100, 70, 80, 120, 80],
-            }]
-          },
-          gradientColors: config.colors.primaryGradient,
-          gradientStops: [1, 0.2, 0],
+              data: [80, 100, 70, 80, 120, 80]
+            }
+          ]
         },
-        greenLineChart: {
-          extraOptions: chartConfigs.greenChartOptions,
-          chartData: {
-            labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV'],
-            datasets: [{
+        gradientColors: config.colors.primaryGradient,
+        gradientStops: [1, 0.2, 0]
+      },
+      greenLineChart: {
+        extraOptions: chartConfigs.greenChartOptions,
+        chartData: {
+          labels: ["JUL", "AUG", "SEP", "OCT", "NOV"],
+          datasets: [
+            {
               label: "My First dataset",
               fill: true,
               borderColor: config.colors.danger,
@@ -192,88 +227,235 @@
               borderDash: [],
               borderDashOffset: 0.0,
               pointBackgroundColor: config.colors.danger,
-              pointBorderColor: 'rgba(255,255,255,0)',
+              pointBorderColor: "rgba(255,255,255,0)",
               pointHoverBackgroundColor: config.colors.danger,
               pointBorderWidth: 20,
               pointHoverRadius: 4,
               pointHoverBorderWidth: 15,
               pointRadius: 4,
-              data: [90, 27, 60, 12, 80],
-            }]
-          },
-          gradientColors: ['rgba(66,134,121,0.15)', 'rgba(66,134,121,0.0)', 'rgba(66,134,121,0)'],
-          gradientStops: [1, 0.4, 0],
+              data: [90, 27, 60, 12, 80]
+            }
+          ]
         },
-        blueBarChart: {
-          extraOptions: chartConfigs.barChartOptions,
-          chartData: {
-            labels: ['USA', 'GER', 'AUS', 'UK', 'RO', 'BR'],
-            datasets: [{
+        gradientColors: [
+          "rgba(66,134,121,0.15)",
+          "rgba(66,134,121,0.0)",
+          "rgba(66,134,121,0)"
+        ],
+        gradientStops: [1, 0.4, 0]
+      },
+      blueBarChart: {
+        extraOptions: chartConfigs.barChartOptions,
+        chartData: {
+          labels: ["USA", "GER", "AUS", "UK", "RO", "BR"],
+          datasets: [
+            {
               label: "Countries",
               fill: true,
               borderColor: config.colors.info,
               borderWidth: 2,
               borderDash: [],
               borderDashOffset: 0.0,
-              data: [53, 20, 10, 80, 100, 45],
-            }]
-          },
-          gradientColors: config.colors.primaryGradient,
-          gradientStops: [1, 0.4, 0],
-        }
+              data: [53, 20, 10, 80, 100, 45]
+            }
+          ]
+        },
+        gradientColors: config.colors.primaryGradient,
+        gradientStops: [1, 0.4, 0]
       }
+    };
+  },
+  computed: {
+    enableRTL() {
+      return this.$route.query.enableRTL;
     },
-    computed: {
-      enableRTL() {
-        return this.$route.query.enableRTL;
-      },
-      isRTL() {
-        return this.$rtl.isRTL;
-      },
-      bigLineChartCategories() {
-        return this.$t('dashboard.chartCategories');
-      }
+    isRTL() {
+      return this.$rtl.isRTL;
     },
-    methods: {
-      initBigChart(index) {
-        let chartData = {
-          datasets: [{
+    bigLineChartCategories() {
+      return this.$t("dashboard.chartCategories");
+    }
+  },
+  methods: {
+    initBigChart(index) {
+      let chartData = {
+        datasets: [
+          {
             fill: true,
             borderColor: config.colors.primary,
             borderWidth: 2,
             borderDash: [],
             borderDashOffset: 0.0,
             pointBackgroundColor: config.colors.primary,
-            pointBorderColor: 'rgba(255,255,255,0)',
+            pointBorderColor: "rgba(255,255,255,0)",
             pointHoverBackgroundColor: config.colors.primary,
             pointBorderWidth: 20,
             pointHoverRadius: 4,
             pointHoverBorderWidth: 15,
             pointRadius: 4,
             data: this.bigLineChart.allData[index]
-          }],
-          labels: ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
-        }
-        this.$refs.bigChart.updateGradients(chartData);
-        this.bigLineChart.chartData = chartData;
-        this.bigLineChart.activeIndex = index;
-      }
-    },
-    mounted() {
-      this.i18n = this.$i18n;
-      if (this.enableRTL) {
-        this.i18n.locale = 'ar';
-        this.$rtl.enableRTL();
-      }
-      this.initBigChart(0);
-    },
-    beforeDestroy() {
-      if (this.$rtl.isRTL) {
-        this.i18n.locale = 'en';
-        this.$rtl.disableRTL();
-      }
+          }
+        ],
+        labels: [
+          "JAN",
+          "FEB",
+          "MAR",
+          "APR",
+          "MAY",
+          "JUN",
+          "JUL",
+          "AUG",
+          "SEP",
+          "OCT",
+          "NOV",
+          "DEC"
+        ]
+      };
+      this.$refs.bigChart.updateGradients(chartData);
+      this.bigLineChart.chartData = chartData;
+      this.bigLineChart.activeIndex = index;
     }
-  };
+  },
+  mounted() {
+    this.i18n = this.$i18n;
+    if (this.enableRTL) {
+      this.i18n.locale = "ar";
+      this.$rtl.enableRTL();
+    }
+    this.initBigChart(0);
+  },
+  beforeDestroy() {
+    if (this.$rtl.isRTL) {
+      this.i18n.locale = "en";
+      this.$rtl.disableRTL();
+    }
+  }
+};
 </script>
 <style>
 </style>
+
+<!--
+
+<template>
+  <div>
+    <card class="text-center">
+      <line-chart
+        style="height: 100%"
+        :chart-data="purpleLineChart.chartData"
+        :gradient-color="purpleLineChart.gradientColors"
+        :gradient-stops="purpleLineChart.gradientStops"
+        :extra-options="purpleLineChart.extraOptions"
+      ></line-chart>
+    </card>
+  </div>
+</template>
+<script>
+import LineChart from "@/components/Charts/LineChart";
+import BarChart from "@/components/Charts/BarChart";
+import {Card} from '@/components'
+//import PieChart from '@/components/Charts/PieChart'
+
+export default {
+  components: {
+    LineChart,
+		BarChart,
+		Card
+    //  PieChart
+  },
+
+  data() {
+    return {
+      purpleLineChart: {
+        extraOptions: {
+          maintainAspectRatio: false,
+          legend: {
+            display: false
+          },
+          responsive: true,
+          tooltips: {
+            backgroundColor: "#f5f5f5",
+            titleFontColor: "#333",
+            bodyFontColor: "#666",
+            bodySpacing: 4,
+            xPadding: 12,
+            mode: "nearest",
+            intersect: 0,
+            position: "nearest"
+          },
+          scales: {
+            yAxes: [
+              {
+                barPercentage: 1.6,
+                gridLines: {
+                  drawBorder: false,
+                  color: "rgba(29,140,248,0.0)",
+                  zeroLineColor: "transparent"
+                },
+                ticks: {
+                  suggestedMin: 50,
+                  suggestedMax: 110,
+                  padding: 20,
+                  fontColor: "#ff8a76"
+                }
+              }
+            ],
+
+            xAxes: [
+              {
+                barPercentage: 1.6,
+                gridLines: {
+                  drawBorder: false,
+                  color: "rgba(220,53,69,0.1)",
+                  zeroLineColor: "transparent"
+                },
+                ticks: {
+                  padding: 20,
+                  fontColor: "#ff8a76"
+                }
+              }
+            ]
+          }
+        },
+        chartData: {
+						labels: [
+						"JAN",
+						"FEB",
+						"MAR",
+						"APR",
+						"MAY",
+						"JUN",
+						"JUL",
+						"AUG",
+						"SEP",
+						"OCT",
+						"NOV",
+						"DEC"
+					],
+          datasets: [
+            {
+              label: "Data",
+              fill: true,
+              borderColor
+: "#d048b6",
+              borderWidth: 2,
+              borderDash: [],
+              borderDashOffset: 0.0,
+              pointBackgroundColor: "#d048b6",
+              pointBorderColor: "rgba(255,255,255,0)",
+              pointHoverBackgroundColor: "#d048b6",
+              pointBorderWidth: 20,
+              pointHoverRadius: 4,
+              pointHoverBorderWidth: 15,
+              pointRadius: 4,
+              data:  [10, 45, 30, 59, 65, 42, 75, 60, 90, 80, 110, 100]
+            }
+          ]
+        }
+      }
+    };
+  }
+};
+</script>
+
+-->
