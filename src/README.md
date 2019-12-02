ee2f1aff35400a9f59e39ebcd1828e66afa5d38a
Wesley Rocha <wesleysr1997@gmail.com>
2019-12-02 10:35:11 -0400

    Telegram bot (#8)
    Add almost complete telegram bot back-end
    
    Still needs to integrate with API

---
+++ src/README.md
@@ -0,0 +1,18 @@
+# Telegram Bot
+
+The telegram bot will be the front-end for the control access system admin.
+
+This is the module which will serve our telegram client.
+
+## What it does (or should do)
+
+The telegram bot back-end will be responsible to:
+
+- [x] communicate with the telegram bot front-end;
+- [ ] connect with the access control system;
+- [x] send the access QR code to the guest via e-mail;
+- [ ] notify the access control system of the sent QR code with it's expiration date;
+- [ ] update the db with gathered data;
+- [ ] display a dashboard.
+
+## Usage
---
+++ src/telegram_bot.py
@@ -0,0 +1,75 @@
+import logging
+
+from telegram import ReplyKeyboardRemove
+from telegram.ext import (
+    CommandHandler,
+    ConversationHandler,
+    Filters,
+    MessageHandler,
+    Updater,
+)
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+GENDER, PHOTO, LOCATION, BIO = range(4)
+
+EMAIL = range(1)
+
+
+def invite(update, context):
+    update.message.reply_text("Digite o e-mail de quem você quer convidar.")
+
+    return EMAIL
+
+
+def email(update, context):
+    email = update.message.text
+    user = update.message.from_user
+    logger.info(f"User {user.first_name} invited {email}")
+
+    reply = "E-mail inválido"
+    if email == "f":
+        reply = "Show!"
+    update.message.reply_text(reply)
+
+    return ConversationHandler.END
+
+
+def cancel(update, context):
+    user = update.message.from_user
+    logger.info(f"User {user.first_name} canceled the operation.")
+
+    update.message.reply_text("Operação cancelada!", reply_markup=ReplyKeyboardRemove())
+
+    return ConversationHandler.END
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning(f"Update {update} caused error {context.error}")
+
+
+def main():
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("convidar", invite)],
+        states={EMAIL: [MessageHandler(Filters.text, email)]},
+        fallbacks=[CommandHandler("cancelar", cancel)],
+    )
+
+    bot_token = input("Insert bot token: ")
+    updater = Updater(bot_token, use_context=True)
+
+    dp = updater.dispatcher
+    dp.add_handler(conv_handler)
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == "__main__":
+    main()
